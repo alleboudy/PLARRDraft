afd49affae4900c1b505a9ac6e9126daf8470efd
alleboudy <ahmad.alleboudy@outloo.com>
2017-04-15 23:03:22 +0200

    tring video camputre

--- pilotTest/src/main.cpp
+++ pilotTest/src/main.cpp
@@ -1,20 +1,45 @@
-#include <stdio.h>
-#include <opencv2/opencv.hpp>
+
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+
 using namespace cv;
 using namespace std;
-int main()
+
+int main(int argc, char* argv[])
 {
-	cout << "hahaha" << endl;
-	Mat image;
-	image = imread("c:\\me.jpg", 1);
-	if (!image.data)
+	VideoCapture cap(0); // open the video camera no. 0
+
+	if (!cap.isOpened()) // if not success, exit program
 	{
-		printf("No image data \n");
+		cout << "Cannot open the video file" << endl;
 		return -1;
 	}
-	namedWindow("Display Image", WINDOW_AUTOSIZE);
-	imshow("Display Image", image);
-	waitKey(0);
-	system("pause");
+
+	double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
+	double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
+
+	cout << "Frame size : " << dWidth << " x " << dHeight << endl;
+
+	namedWindow("MyVideo", CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
+	Mat frame;
+
+	while (1)
+	{
+		bool bSuccess = cap.read(frame); // read a new frame from video
+
+		if (!bSuccess) //if not success, break loop
+		{
+			cout << "Cannot read a frame from video file" << endl;
+			break;
+		}
+
+		imshow("MyVideo", frame); //show the frame in "MyVideo" window
+
+		if (waitKey(30) == 27) //wait for ‘esc’ key press for 30ms. If ‘esc’ key is pressed, break loop
+		{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}
+	}
 	return 0;
 }
